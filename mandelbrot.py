d56303894ea54a7e30d99c6019076f068648ffc3
Atharva Ahire <me.3.141.pi@gmail.com>
2025-09-05 11:43:22 +0530

    Update mandelbrot.py
    Added divergence image feature and divergence zoom sequence feature (divergence image = indicates how fast each complex number diverges)

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,8 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import colorsys
 from PIL import Image
 import numba as nb
 import sys
+import math
+import pickle
+
+
+
+with open("color_list.pkl", "rb") as f:
+    color_list = pickle.load(f)
+def distinct_colors(n):
+    return color_list[0:n]
+
 
 def complex_matrix(corner1=-1 + 1j, corner2=1 - 1j, x_num=100, y_num=100):
     x_s = np.linspace(corner1.real, corner2.real, num=x_num)
@@ -23,16 +34,20 @@ class mandelbrot():
         self.explosion_boundary = explosion_boundary
         self.mask = np.ones_like(self.matrix,dtype = bool)
         self.corners = (corner1 , corner2)
+        self.counter = np.zeros_like(self.matrix,dtype = int)
     def mandelbrot_validity(self,z):
         return np.abs(z)<self.explosion_boundary
 
     def run(self,iter):
         z = np.zeros_like(self.matrix)
         c  = self.matrix[:]
+        count = np.zeros_like(self.matrix)
         for _ in range(iter):
 
             z[self.mask] = z[self.mask]**self.power + c[self.mask]
             self.mask = self.mandelbrot_validity(z)
+            count[self.mask] = count[self.mask] + 1
+        self.counter = count
     def reset(self):
         self.mask = np.ones_like(self.matrix,dtype = bool)
     def plot_mandelbrot(self):
@@ -42,17 +57,34 @@ class mandelbrot():
         plt.show()
 
     #Saves the mandelbrot
-    def image(self):
+    def image(self,color = None,colorBG=None):
+        if color is None:
+            color = (255,255,255)
+        if colorBG is None:
+            colorBG = (0,0,0)
         h, b = self.matrix.shape
-        img = Image.new(mode="RGB", size=(b,h), color=(0, 0, 0))
+        img = Image.new(mode="RGB", size=(b,h), color=colorBG)
         for i in range(h):
             for j in range(b):
-                cl = self.mask[i][j]*255
-                img.putpixel((j,i),(cl,cl,cl))
+                if (self.mask[i][j])    :
+                    img.putpixel((j,i),color)
+        return img
+
+    def divergence_image(self,color_n = 30):
+        h, b = self.matrix.shape
+        img = Image.new(mode="RGB", size=(b, h), color=(0, 0, 0))
+        max_iter = np.max(self.counter)
+
+        for i in range(h):
+            for j in range(b):
+                ratio = self.counter[i][j] / (max_iter + 1)
+                #print(ratio)
+                col_temp = tuple(color_list[math.floor(ratio * color_n)])
+                img.putpixel((j, i), col_temp)
         return img
 
     #generates the mandelbrot in batches rather than making it all at once
-    def stitch_image(self,patch_dim = 100 , patch_num_x = 5 , patch_num_y = 5,iter_per_patch = 200):
+    def stitch_image(self,patch_dim = 100 , patch_num_x = 5 , patch_num_y = 5,iter_per_patch = 200,color = None,colorBG = None):
         super_img = Image.new(mode="RGB", size=(patch_num_x * patch_dim, patch_num_y* patch_dim), color=(0, 0, 0))
         boundary_vertices = complex_matrix(self.corners[0],self.corners[1],x_num=patch_num_x+1,y_num=patch_num_y+1)
         indx = 0
@@ -62,7 +94,7 @@ class mandelbrot():
                 c1,c2 = boundary_vertices[j][i],boundary_vertices[j+1][i+1]
                 temp = mandelbrot(c1,c2,x_num=patch_dim,y_num=patch_dim,power=self.power,explosion_boundary=self.explosion_boundary)
                 temp.run(iter=iter_per_patch)
-                temp = temp.image()
+                temp = temp.image(color = color,colorBG = colorBG)
                 super_img.paste(temp,(i*patch_dim,j*patch_dim))
                 del temp
                 sys.stdout.write(f'\rProgress: {indx*100/total}%')
@@ -71,7 +103,8 @@ class mandelbrot():
         return  super_img
 
     def zoom_sequence(self,n_frames = 100,zoom_center = 0,zoom_per_frame = 0.95,
-                 frame_iter = 100,frame_x_num = 500, frame_y_num = 500,corner1=None, corner2=None):
+                 frame_iter = 100,frame_x_num = 500, frame_y_num = 500,corner1=-1+1j, corner2=1-1j,
+                      color = None,colorBG = None):
         if corner1 is None:
             corner1 = self.corners[0]
         if corner2 is None:
@@ -82,15 +115,43 @@ class mandelbrot():
         for _ in range(n_frames):
             temp = mandelbrot(corner1,corner2,x_num=frame_x_num,y_num=frame_y_num,power=self.power,explosion_boundary=self.explosion_boundary)
             temp.run(iter = frame_iter)
-            f = temp.image()
+            f = temp.image(color = color,colorBG = colorBG)
             frames.append(f)
-            del(temp)
+            del temp
+            sys.stdout.write(f"\rProgress: {_ * 100 / n_frames:.2f}%")
+            sys.stdout.flush()
             corner1 = (corner1 - zoom_center) * zoom_per_frame + zoom_center
             corner2 = (corner2 - zoom_center) * zoom_per_frame + zoom_center
             #print(_)
         return frames
+
+    def zoom_sequence_divergence(self, n_frames=100, zoom_center=0, zoom_per_frame=0.95,
+                                 frame_iter=100, frame_x_num=500, frame_y_num=500,
+                                 corner1=None, corner2=None, color_n=30):
+        if corner1 is None:
+            corner1 = self.corners[0]
+        if corner2 is None:
+            corner2 = self.corners[1]
+
+        frames = []
+
+        for _ in range(n_frames):
+            temp = mandelbrot(corner1, corner2, x_num=frame_x_num, y_num=frame_y_num,
+                              power=self.power, explosion_boundary=self.explosion_boundary)
+            temp.run(iter=frame_iter)
+            f = temp.divergence_image(color_n=color_n)
+            frames.append(f)
+            del temp
+            sys.stdout.write(f"\rProgress: {_ * 100 / n_frames:.2f}%")
+            sys.stdout.flush()
+            corner1 = (corner1 - zoom_center) * zoom_per_frame + zoom_center
+            corner2 = (corner2 - zoom_center) * zoom_per_frame + zoom_center
+
+        return frames
+
     def zoom_sequence_patched(self, n_frames=100, zoom_center=0, zoom_per_frame=0.95,
-                          frame_iter=100, patch_dim = 100,patch_num_x = 5,patch_num_y = 5, corner1=None, corner2=None):
+                          frame_iter=100, patch_dim = 100,patch_num_x = 5,patch_num_y = 5, corner1=None, corner2=None,
+                              color = None,colorBG = None):
         if corner1 is None:
             corner1 = self.corners[0]
         if corner2 is None:
@@ -100,25 +161,16 @@ class mandelbrot():
         for _ in range(n_frames):
             temp = mandelbrot(corner1, corner2, power=self.power,
                               explosion_boundary=self.explosion_boundary)
-            f = temp.stitch_image(patch_dim = patch_dim,patch_num_x = patch_num_x , patch_num_y = patch_num_y , iter_per_patch = frame_iter)
+            f = temp.stitch_image(patch_dim = patch_dim,patch_num_x = patch_num_x , patch_num_y = patch_num_y , iter_per_patch = frame_iter,color = color,colorBG = colorBG)
             frames.append(f)
-            del(temp)
+            del temp
             corner1 = (corner1 - zoom_center) * zoom_per_frame + zoom_center
             corner2 = (corner2 - zoom_center) * zoom_per_frame + zoom_center
             #print(_)
 
         return frames
     
-# diagonal=  (-0.25 + 0.25j)
-# center = -0.7435669 + 0.1314023j
-# c1 , c2 = center + diagonal, center - diagonal
-# frames = mandelbrot().zoom_sequence_patched(corner1 = c1 , corner2 = c2,zoom_center = center,n_frames = 500,zoom_per_frame = 0.98,patch_dim = 200)
-# frames[0].save(
-#     "mfffe.gif",
-#     save_all=True,
-#     append_images=frames[1:],  # the rest of the frames
-#     optimize=False,            # you can try True for smaller file size
-#     duration=50,               # time per frame in ms (20 fps = 50ms)
-#     loop=0                     # 0 = loop forever
 
-# )
+
+
+
